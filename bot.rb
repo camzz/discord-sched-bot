fcd4d573b72c4dc85fe69e6132677e5bb41e323e
Tim Spence <timothywspence@gmail.com>
2017-05-08 13:41:05 +0100

    Removed duplication and added delete event functionality

--- bot.rb
+++ bot.rb
@@ -43,47 +43,52 @@ def handle_create(event, args)
   event.respond "New event #{name} scheduled for #{time}"
 end
 
+#TODO remove past events
 def handle_list(event, args)
   event.respond $store.values.join('\r\n')
 end
 
 def handle_yes(event, args)
-  event_id = args[1]
-  scheduled = $store[event_id]
-  if scheduled
+  handle_missing(event, args) do |scheduled|
     scheduled.accepted.push(event.user.username)
-  else
-    event.respond "No event found with id #{event_id}"
+    nil
   end
 end
 
 def handle_no(event, args)
-  event_id = args[1]
-  scheduled = $store[event_id]
-  if scheduled
+  handle_missing(event, args) do |scheduled|
     scheduled.declined.push(event.user.username)
-  else
-    event.respond "No event found with id #{event_id}"
+    nil
   end
 end
 
 def handle_maybe(event, args)
-  event_id = args[1]
-  scheduled = $store[event_id]
-  if scheduled
+  handle_missing(event, args) do |scheduled|
     scheduled.maybe.push(event.user.username)
-  else
-    event.respond "No event found with id #{event_id}"
+    nil
+  end
+end
+
+def handle_delete(event, args)
+  handle_missing(event, args) do |scheduled|
+    $store.delete(scheduled.id)
+    nil
   end
 end
 
 #TODO extract this logic to method which takes block
 ##TODO accepting should remove from list of declines if necessary
 def handle_responses(event, args)
+  handle_missing(event, args) do |scheduled|
+    scheduled.responses
+  end
+end
+
+def handle_missing(event, args, &block)
   event_id = args[1]
   scheduled = $store[event_id]
   if scheduled
-    event.respond scheduled.responses
+    event.respond block[scheduled]
   else
     event.respond "No event found with id #{event_id}"
   end
@@ -97,6 +102,8 @@ list
   list all registered future events
 create name time
   Create a new event at the given time
+delete id
+  Delete the event with the given id
 accept|yes id
   Register for the event with the given id
 decline|no id
@@ -118,12 +125,16 @@ bot.command :sched do |event, *args|
   case args.first
   when 'create'
     handle_create(event, args)
+  when 'delete'
+    handle_delete(event, args)
   when 'list'
     handle_list(event, args)
   when /accept|yes/
     handle_yes(event, args)
   when /decline|no/
     handle_no(event, args)
+  when 'maybe'
+    handle_maybe(event, args)
   when 'help'
     handle_help(event, args)
   when 'responses'
