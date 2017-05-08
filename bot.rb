d7fda979955e76590fc300bd88c76b70b9a4e4eb
Tim Spence <timothywspence@gmail.com>
2017-05-08 12:34:24 +0100

    Sketch of commands

--- bot.rb
+++ bot.rb
@@ -1,4 +1,77 @@
 require 'discordrb'
+require 'securerandom'
+require 'time'
+
+$store = {}
+
+class Event
+
+  def initialize(id, name, time)
+    @id = id
+    @name = name
+    @time = time
+    @attendees = []
+  end
+
+  def to_s
+    "#{@name}(ID #{@id}) scheduled for #{@time}"
+  end
+
+end
+
+class Attendee
+
+  def new()
+  end
+
+end
+
+def handle_create(event, args)
+  puts "Passed args #{args}"
+  id = SecureRandom.uuid
+  name = args[1]
+  puts "Name is #{name}"
+  time = Time.parse(args[2])
+  $store[id] = Event.new(id, name, time)
+  puts "Time is #{time}"
+  puts "responding"
+  event.respond "New event #{name} scheduled for #{time}"
+end
+
+def handle_list(event, args)
+  event.respond $store.values.join('\r\n')
+end
+
+def handle_yes(event, args)
+  event_id = args[1]
+
+end
+
+def handle_no(event, args)
+  event_id = args[1]
+
+end
+
+def handle_maybe(event, args)
+
+end
+
+def handle_help(event, args)
+  event.respond <<~EOF
+Usage: !sched <COMMAND> <ARGS>
+where <COMMAND> one of:
+list
+  list all registered future events
+create name time
+  Create a new event at the given time
+accept|yes id
+  Register for the event with the given id
+decline|no id
+  Decline the event with the given id
+maybe
+  Sit on the fence for the event with the given id (Don't be that guy!)
+  EOF
+end
 
 # This statement creates a bot with the specified token and application ID. After this line, you can add events to the
 # created bot, and eventually run it.
@@ -9,19 +82,26 @@ require 'discordrb'
 # you, look here: https://github.com/meew0/discordrb/wiki/Redirect-URIs-and-RPC-origins
 # After creating the bot, simply copy the token (*not* the OAuth2 secret) and the client ID and put it into the
 # respective places.
-bot = Discordrb::Bot.new token: 'XXX', client_id: 311083321994248192
+bot = Discordrb::Commands::CommandBot.new token: 'XXX', client_id: 311083321994248192, prefix: '!'
 
-# Here we output the invite URL to the console so the bot account can be invited to the channel. This only has to be
-# done once, afterwards, you can remove this part if you want
 puts "This bot's invite URL is #{bot.invite_url}."
 puts 'Click on it to invite it to your server.'
 
-# This method call adds an event handler that will be called on any message that exactly contains the string "Ping!".
-# The code inside it will be executed, and a "Pong!" response will be sent to the channel.
-bot.message(content: 'Ping!') do |event|
-  event.respond 'Pong!'
+bot.command :sched do |event, *args|
+  puts args
+  case args.first
+  when 'create'
+    handle_create(event, args)
+  when 'list'
+    handle_list(event, args)
+  when /accept|yes/
+    handle_yes(event, args)
+  when /decline|no/
+    handle_no(event, args)
+  when 'help'
+    handle_help(event, args)
+  else event.respond "Command not recognised"
+  end
 end
 
-# This method call has to be put at the end of your script, it is what makes the bot actually connect to Discord. If you
-# leave it out (try it!) the script will simply stop and the bot will not appear online.
 bot.run
